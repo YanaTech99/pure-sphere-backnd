90368c9cad0b74e72254ce4473ac37817e6ac110
YanaTech99 <yanatechdigital@gmail.com>
2026-02-21 16:29:00 +0530

    Backend update

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,7 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
 
+dotenv.config();
 const redis = process.env.REDIS_URL
   ? createClient({ url: process.env.REDIS_URL })
   : null;
